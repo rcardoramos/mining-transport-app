9a759f87a1ca20223ae27b650f3ada6ed4631ba2
rcardoramos <rcardoramos26@gmail.com>
2026-08-05 15:58:39 -0500

    revert: remove MobileScannerController formats property to resolve camera stream initialization failures

--- lib/core/scanner/qr_scanner_page.dart
+++ lib/core/scanner/qr_scanner_page.dart
@@ -12,12 +12,7 @@ class QrScannerPage extends StatefulWidget {
 }
 
 class _QrScannerPageState extends State<QrScannerPage> with SingleTickerProviderStateMixin {
-  final MobileScannerController controller = MobileScannerController(
-    formats: [
-      BarcodeFormat.pdf417,
-      BarcodeFormat.qrCode,
-    ],
-  );
+  final MobileScannerController controller = MobileScannerController();
   late AnimationController _animationController;
   late Animation<double> _animation;
 
